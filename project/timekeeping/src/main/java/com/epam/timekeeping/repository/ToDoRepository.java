d10704940ad0e4de30a83529c7f344c7a857d4cf
Daniel Aguirre1 <daniel_aguirre1@epam.com>
2022-08-24 13:45:34 -0500

    ready to migrate to springboot

--- project/timekeeping/src/main/java/com/epam/timekeeping/repository/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epam.timekeeping.repository;
-
-import com.epam.timekeeping.domain.ToDo;
-import org.springframework.data.repository.CrudRepository;
-public interface ToDoRepository extends CrudRepository<ToDo,String> {
-
-}
